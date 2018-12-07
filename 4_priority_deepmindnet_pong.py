27353a2a1a2c5b13fea6c36158b4783c0567c7e2
lzhan144 <43391910+lzhan144@users.noreply.github.com>
2018-12-07 14:02:03 +0800

    Update 4_priority_deepmindnet_pong.py

--- 4_priority_deepmindnet_pong.py
+++ 4_priority_deepmindnet_pong.py
@@ -718,7 +718,7 @@ env = wrap_deepmind(env)
 
 action_size = env.action_space.n
 agent = DQNAgent(action_size)
-PATH = "/Playing-Atari-DQN/4_frame_Pong_raw_pixel_linear_ultimate.pt"
+PATH = "Playing-Atari-DQN/4_frame_Pong_raw_pixel_linear_ultimate.pt"
 agent.model=torch.load(PATH)
 agent.train_start = 1
 
